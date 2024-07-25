39ceba814c6fa485e6ad3b789ceca875b3b4d72a
SUMMERxKx <samar.k.khajuria@gmail.com>
2024-07-24 23:24:36 -0700

    Event Project

---
+++ UnlimitedColors/script.js
@@ -0,0 +1,27 @@
+//geerating random color 
+const randomColor = () => {
+    let hex = '0123456789ABCDEF';
+    let color = "#";
+    for (let i = 0; i < 6;i++){
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+    let intervalID;
+
+const startChangingColor = () => {
+    intervalID = setInterval(changeBg, 2000)
+
+    function changeBg (){ 
+    document.body.style.backgroundColor = randomColor();
+}
+    
+}
+
+const stopChangingColor = () => {
+    clearInterval(intervalID)
+
+}
+document.getElementById("start").addEventListener("click", startChangingColor,)
+document.getElementById("stop").addEventListener("click", stopChangingColor)
+
